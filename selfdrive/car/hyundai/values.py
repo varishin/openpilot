976d7ff0648f91bdeb7aa1c2d07e7ea756899ae5
xps-genesis <65239463+xps-genesis@users.noreply.github.com>
2020-09-01 08:23:09 -0400

    add kia niroHEV and sportage to ignore fingreprints because of identical FP.
    should use FPV2

--- selfdrive/car/hyundai/values.py
+++ selfdrive/car/hyundai/values.py
@@ -159,7 +159,7 @@ ECU_FINGERPRINT = {
 }
 
 # Don't use these fingerprints for fingerprinting, they are still used for ECU detection
-IGNORED_FINGERPRINTS = []
+IGNORED_FINGERPRINTS = [CAR.KIA_NIRO_HEV, KIA_SPORTAGE]
 
 FW_VERSIONS = {
   CAR.SONATA: {
