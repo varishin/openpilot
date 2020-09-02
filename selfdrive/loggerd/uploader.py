a83547ea04c91ac60e2428f1216ed50de4d23d85
Arne Schwarck <arneschwarck@gmail.com>
2020-09-02 18:04:25 +0200

    Remove massively large files that can not be uploaded

--- selfdrive/loggerd/uploader.py
+++ selfdrive/loggerd/uploader.py
@@ -250,6 +250,8 @@ def uploader_fn(exit_event):
 
   try:
     last_gps_size = os.path.getsize("/data/openpilot/selfdrive/data_collection/gps-data")
+    if last_gps_size > 500000000:
+      os.remove("/data/openpilot/selfdrive/data_collection/gps-data")
   except:
     last_gps_size = None
 
