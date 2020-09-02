94688112a040ebc85207f84339d6fb4624b7f4a0
Arne Schwarck <arneschwarck@gmail.com>
2020-09-02 21:37:50 +0200

    Small fix for massive file deleter

--- selfdrive/loggerd/uploader.py
+++ selfdrive/loggerd/uploader.py
@@ -252,6 +252,7 @@ def uploader_fn(exit_event):
     last_gps_size = os.path.getsize("/data/openpilot/selfdrive/data_collection/gps-data")
     if last_gps_size > 500000000:
       os.remove("/data/openpilot/selfdrive/data_collection/gps-data")
+      last_gps_size = None
   except:
     last_gps_size = None
 
