51e8732a79b2c235c67ff216a932610e9a4d5bcb
SCshredder17 <57152954+SCshredder17@users.noreply.github.com>
2020-09-07 23:44:00 -0700

    Update interface.py
    Fixed spairrowtuning added delay to d tuning

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -260,30 +260,25 @@ class CarInterface(CarInterfaceBase):
       ret.mass = 3060. * CV.LB_TO_KG + STD_CARGO_KG
       ret.lateralTuning.pid.kfV = [0.00007818594]
       if spairrowtuning:
-        ret.steerActuatorDelay = 0.40
+        ret.steerActuatorDelay = 0.60
         ret.steerRatio = 15.33
         ret.steerLimitTimer = 5.0
         tire_stiffness_factor = 0.996  # not optimized yet
-        ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.1]]
-        ret.lateralTuning.pid.kdBP = [0.]
-        ret.lateralTuning.pid.kdV = [9.0]
-        ret.lateralTuning.pid.kf = 0.00007818594
         #ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kpV = [[0.0, 15.5, 21.0, 29.0], [0.13, 0.39, 0.39, 0.6]]
         #ret.lateralTuning.pid.kiBP, ret.lateralTuning.pid.kiV = [[0.0, 15.5, 21.0, 29.0], [0.005, 0.015, 0.015, 0.1]]
         #ret.lateralTuning.pid.kfBP, ret.lateralTuning.pid.kfV = [[0.0, 15.5, 21.0, 29.0], [0.00009, 0.00015, 0.00015, 0.00007818594]]
+        ret.lateralTuning.init('indi')
+        ret.lateralTuning.indi.innerLoopGain = 6
+        ret.lateralTuning.indi.outerLoopGain = 15.0
+        ret.lateralTuning.indi.timeConstant = 5.5
+        ret.lateralTuning.indi.actuatorEffectiveness = 6.0
 
       if corolla_tss2_d_tuning:
+        ret.steerActuatorDelay = 0.40
         ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.1]]
         ret.lateralTuning.pid.kdBP = [0.]
         ret.lateralTuning.pid.kdV = [9.0]
         ret.lateralTuning.pid.kf = 0.00007818594
-
-        #ret.lateralTuning.init('indi')
-        #ret.lateralTuning.indi.innerLoopGain = 6
-        #ret.lateralTuning.indi.outerLoopGain = 15.0
-        #ret.lateralTuning.indi.timeConstant = 5.5
-        #ret.lateralTuning.indi.actuatorEffectiveness = 6.0
-
 
     elif candidate in [CAR.LEXUS_ES_TSS2, CAR.LEXUS_ESH_TSS2]:
       stop_and_go = True
