8510c8b1a7f15fcc59144ec207f12ad546a77043
rav4kumar <36933347+rav4kumar@users.noreply.github.com>
2020-09-12 21:13:51 -0700

    this doesnt need to be merged. I am just testing how i can reduce temp with just eon fan.

--- selfdrive/thermald/thermald.py
+++ selfdrive/thermald/thermald.py
@@ -114,11 +114,11 @@ def set_eon_fan(val):
 
 
 # temp thresholds to control fan speed - high hysteresis
-_TEMP_THRS_H = [50., 65., 80., 10000]
+_TEMP_THRS_H = [45., 55., 70., 10000]
 # temp thresholds to control fan speed - low hysteresis
-_TEMP_THRS_L = [42.5, 57.5, 72.5, 10000]
+_TEMP_THRS_L = [37.5, 45.5, 55.5, 10000]
 # fan speed options
-_FAN_SPEEDS = [0, 16384, 32768, 65535]
+_FAN_SPEEDS = [0, 32768, 32768, 65535]
 # max fan speed only allowed if battery is hot
 _BAT_TEMP_THERSHOLD = 45.
 
@@ -144,7 +144,7 @@ def handle_fan_eon(max_cpu_temp, bat_temp, fan_speed, ignition):
 
 
 def handle_fan_uno(max_cpu_temp, bat_temp, fan_speed, ignition):
-  new_speed = int(interp(max_cpu_temp, [40.0, 80.0], [0, 80]))
+  new_speed = int(interp(max_cpu_temp, [35.0, 80.0], [0, 80]))
 
   if not ignition:
     new_speed = min(30, new_speed)
@@ -315,7 +315,7 @@ def thermald_thread():
         ip_addr = 'N/A'
       ts_last_ip = ts
       #msg2 = messaging_arne.new_message('ipAddress')
-      
+
       #arne_pm.send('ipAddress', msg2)
     msg.thermal.ipAddr = ip_addr
 
