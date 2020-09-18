88678b3f711eb642da88010a650364de0901b642
Arne Schwarck <arneschwarck@gmail.com>
2020-09-18 15:05:05 +0200

    Fix pid being set with init

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -287,9 +287,7 @@ class CarInterface(CarInterfaceBase):
       ret.wheelbase = 2.63906
       ret.steerRatio = 13.9
       tire_stiffness_factor = 0.444  # not optimized yet
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.5], [0.1]]
       ret.mass = 3060. * CV.LB_TO_KG + STD_CARGO_KG
-      ret.lateralTuning.pid.kfV = [0.00007818594]
       if spairrowtuning:
         ret.steerActuatorDelay = 0.60
         ret.steerRatio = 15.33
@@ -303,13 +301,13 @@ class CarInterface(CarInterfaceBase):
         ret.lateralTuning.indi.outerLoopGain = 15.0
         ret.lateralTuning.indi.timeConstant = 5.5
         ret.lateralTuning.indi.actuatorEffectiveness = 6.0
-
-      if corolla_tss2_d_tuning:
-        ret.steerActuatorDelay = 0.40
-        ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.1]]
-        ret.lateralTuning.pid.kdBP = [0.]
-        ret.lateralTuning.pid.kdV = [9.0]
-        ret.lateralTuning.pid.kf = 0.00007818594
+      else:
+        ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.5], [0.1]]
+        ret.lateralTuning.pid.kfV = [0.00007818594]
+        if corolla_tss2_d_tuning:
+          ret.steerActuatorDelay = 0.40
+          ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.6], [0.1]]
+          ret.lateralTuning.pid.kdV = [9.0]
         
     elif candidate in [CAR.LEXUS_ES_TSS2, CAR.LEXUS_ESH_TSS2]:
       stop_and_go = True
