3a1c15fe09b1620f2af483a1cd9d1001938807ca
Kumar <36933347+rav4kumar@users.noreply.github.com>
2020-09-18 18:23:19 -0700

    Update longcontrol.py

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -89,7 +89,7 @@ class LongControl():
       elif gas_button_status == 1:
         y = [0.2, 0.45, 0.7]
       elif gas_button_status == 2:
-        y = [0.2, 0.35, 0.66]
+        y = [0.2, 0.3, 0.5]
     else:
       if gas_button_status == 0:
         dynamic = True
