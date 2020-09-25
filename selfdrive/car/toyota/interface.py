b68bb91222dde97ee62999ad5f15c59a3c9f36f1
Prius21Kumar <rav4kumar@users.noreply.github.com>
2020-09-25 06:53:58 -0700

    it should match dbc now.

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -384,7 +384,7 @@ class CarInterface(CarInterfaceBase):
 
     elif candidate == CAR.PRIUS_TSS2:
       stop_and_go = True
-      ret.safetyParam = 73
+      ret.safetyParam = 50
       ret.wheelbase = 2.70
       ret.steerRatio = 15.5   # guess work
       tire_stiffness_factor = 0.6371   # hand-tune
