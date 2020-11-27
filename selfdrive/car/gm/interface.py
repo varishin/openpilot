487b0a6874969c3a4fecf6ec9cb83e6d28d13beb
linu1983 <linu1983@gmail.com>
2020-11-27 19:30:58 +0900

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -199,7 +199,7 @@ class CarInterface(CarInterfaceBase):
 
     ret.buttonEvents = buttonEvents
     
-    if cruiseEnabled and self.CS.lka_button and self.CS.lka_button != self.CS.prev_lka_button:
+    if self.CS.lka_button and self.CS.lka_button != self.CS.prev_lka_button:
       self.CS.lkMode = not self.CS.lkMode
       self.CS.autoHold = not self.CS.autoHold
       if self.CS.autoHold:
