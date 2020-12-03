32cd1e0378402c9daef6f4a6ebfceb6e4e5508c1
linu1983 <linu1983@gmail.com>
2020-12-03 22:53:45 +0900

    Update kegman_conf.py

--- selfdrive/kegman_conf.py
+++ selfdrive/kegman_conf.py
@@ -119,7 +119,7 @@ class kegman_conf():
         self.element_updated = True
 
       if "AutoHold" not in self.config:
-        self.config.update({"AutoHold":"0"})
+        self.config.update({"AutoHold":"1"})
         self.element_updated = True
 
       if "CruiseDelta" not in self.config:
@@ -145,7 +145,7 @@ class kegman_conf():
     else:
       self.config = {"cameraOffset":"0.06", "lastTrMode":"1", "battChargeMin":"60", "battChargeMax":"70", \
                      "wheelTouchSeconds":"180", "accelerationMode":"1","battPercOff":"25", "carVoltageMinEonShutdown":"11800", \
-                     "brakeStoppingTarget":"0.25", "tuneGernby":"1", "AutoHold":"0",\
+                     "brakeStoppingTarget":"0.25", "tuneGernby":"1", "AutoHold":"1",\
                      "Kp":"0.125", "Ki":"0.02", "liveParams":"1", "leadDistance":"5", "deadzone":"0.0", \
                      "1barBP0":"-0.1", "1barBP1":"2.25", "2barBP0":"-0.1", "2barBP1":"2.5", "3barBP0":"0.0", \
                      "3barBP1":"3.0", "1barMax":"2.1", "2barMax":"2.1", "3barMax":"2.1", \
