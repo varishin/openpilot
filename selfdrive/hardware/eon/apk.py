0ece77414d07a1034b9f80b2a32e810c7bc0e684
linu1983 <linu1983@gmail.com>
2020-12-22 21:50:59 +0900

    Update apk.py

--- selfdrive/hardware/eon/apk.py
+++ selfdrive/hardware/eon/apk.py
@@ -6,7 +6,7 @@ import shutil
 from common.basedir import BASEDIR
 from selfdrive.swaglog import cloudlog
 
-android_packages = ("ai.comma.plus.offroad", "com.neokii.oproadlimit")
+android_packages = ("ai.comma.plus.offroad", "com.neokii.openpilot", "com.neokii.oproadlimit")
 
 def get_installed_apks():
   dat = subprocess.check_output(["pm", "list", "packages", "-f"], encoding='utf8').strip().split("\n")
@@ -29,6 +29,8 @@ def install_apk(path):
 def start_offroad():
   set_package_permissions()
   system("am start -n ai.comma.plus.offroad/.MainActivity")
+  pm_grant("com.neokii.openpilot", "android.permission.ACCESS_FINE_LOCATION")
+  system("am startservice com.neokii.openpilot/.MainService")  
   system("am startservice com.neokii.oproadlimit/.MainService")
 
 def set_package_permissions():
